e0ff63be1b851a332ad048cd08343b2d596a5478
Jake Linn <jake_linn@Jakes-MacBook-Air-2.local>
2021-04-01 21:17:52 -0400

    forms working

--- sign-in/src/App.js
+++ sign-in/src/App.js
@@ -1,53 +1,33 @@
-import './App.css';
-import styled from 'styled-components';
-import Form from './Form'
-import {Switch, Route, Link} from 'react-router-dom'
-import SignUp from './Sign-up'
-import Home from './Home'
+import "./App.css";
+import styled from "styled-components";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import SignUp from "./components/SignUp";
+import SignIn from "./components/SignIn"
 
-const styledSignUp = styled.div `
-h1{
-margin: 30px;
-font-size: 45%;
-color: grey
-
-}
-
-`
+const styledSignUp = styled.div`
+  h1 {
+    margin: 30px;
+    font-size: 45%;
+    color: grey;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-    
-      <styledSignUp>
-      
-        
-        <button disabled > About Us? </button>
-        
-        <h1>Sign In </h1>
+    <styledSignUp>
+      <Router>
+        <Switch>
+          
+          <Route exact path="/" component={Home} />
+          
+          <Route exact path="/SignUp" component={SignUp} />
 
+          <Route exact path="/SignIn" component={SignIn} />
 
-      
-        <Form/>
-
-        <footer> 
-        <h3> Don't have an account? Create </h3>
-
-        <Link style  = {{textDecoration: 'none'}} to  = '/Sign-Up' > <p>Sign Up</p> </Link>
-
-        <Link style  = {{textDecoration: 'none'}} to  = '/Home' > <p> Home </p> </Link>
-
-         <Switch> 
-        <Route   path = '/Sign-Up' component = {SignUp} />
-
-        <Route exact path = '/' component = {Home} />
-        </Switch> 
-        </footer>
-
-        </styledSignUp>
-      
-
-    </div>
+        </Switch>
+      </Router>
+    </styledSignUp>
   );
 }
 
--- sign-in/src/index.js
+++ sign-in/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App'
-import Home from './Home';
+import App from './App'
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router> <Home /> </Router>
+    <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
